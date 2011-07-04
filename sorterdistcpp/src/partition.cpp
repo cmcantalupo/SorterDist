31ee75a48c02b0ba651abf5574696b5ffe193fcf
cmcantalupo <cmcantalupo@gmail.com>
2011-07-04 13:11:49 -0700

    Added constructor and destructor to partition

--- sorterdistcpp/src/partition.cpp
+++ sorterdistcpp/src/partition.cpp
@@ -1,16 +1,28 @@
-#include "sorter_threaded.hpp"
 #include "partition.hpp"
 
 namespace SorterThreadedHelper {
  
-  Partition::Partition(const std::set<double>::iterator pivotBegin,
-                       const std::set<double>::iterator pivotEnd,
+  Partition::Partition(const std::set<double>::iterator pivotsBegin,
+                       const std::set<double>::iterator pivotsEnd,
                        const std::vector<double>::iterator chunkBegin,
-                       const std::vector<double>::iterator chunkEnd) {
+                       const std::vector<double>::iterator chunkEnd) :
+    chunkBegin_(chunkBegin), chunkEnd_(chunkEnd) {
   
+    for (std::set<double>::iterator it = pivotsBegin; it != pivotsEnd; ++it) { 
+      partition_.insert(new PartitionWall(*it, false));
+    }
+    partition_.insert(new PartitionWall(*pivotsBegin, true));
   }
+  Partition::~Partition() {
+    for (std::set<PartitionWall*>::iterator it = partition_.begin(); it != partition_.end(); ++it) {
+      delete *it;
+    }
+  }
+
+
 }
                                 
+
 
 
 
