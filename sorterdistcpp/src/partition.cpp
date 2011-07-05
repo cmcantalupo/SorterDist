b73a840fdac4d751ccc970e0bfd7c5b5d50bea45
cmcantalupo <cmcantalupo@gmail.com>
2011-07-04 20:07:26 -0700

    fixed up the map

--- sorterdistcpp/src/partition.cpp
+++ sorterdistcpp/src/partition.cpp
@@ -8,25 +8,30 @@ namespace SorterThreadedHelper {
                        const std::vector<double>::iterator chunkEnd) :
     chunkBegin_(chunkBegin), chunkEnd_(chunkEnd) {
   
+    std::pair<PartitionWall,std::stack<double>*> pp;
     for (std::set<double>::iterator it = pivots.begin(); it != pivots.end(); ++it) {
-      partition_.insert(std::pair<PartitionWall,std::stack<double>*>(PartitionWall(*it,false), new stack<double>));
+      pp.first = PartitionWall::PartitionWall(*it,false);
+      pp.second = new std::stack<double>;
+      partition_.insert(pp);
     }
-    partition_.insert(std::pair<PartitionWall,std::stack<double>*>(PartitionWall(*pivots.begin(), true), new stack<double>));
+    pp.first = PartitionWall::PartitionWall(*pivots.begin(), true);
+    pp.second = new std::stack<double>;
+    partition_.insert(pp);
   }
   Partition::~Partition() {
-    for (std::map<PartitionWall,std::stack<double>*>:iterator it = partition_.begin(); it != partition_.end(); ++it) {
-      delete it.second();
+    for (std::map<PartitionWall,std::stack<double>*>::iterator it = partition_.begin(); it != partition_.end(); ++it) {
+      delete it->second;
     }
   }
      
 
   void Partition::fillPartition() {
-    std::set<double>::iterator ub;
+    std::map<PartitionWall,std::stack<double>*>::iterator ub;
     for (std::vector<double>::iterator it = chunkBegin_;
          it != chunkEnd_; ++it) {
-      ub = partition_.upper_bound(PartitionWall(*it, false));
+      ub = partition_.upper_bound(PartitionWall::PartitionWall(*it,false));
       if (ub == partition_.end()) --ub;
-      ub->second()->push(*it);
+      ub->second->push(*it);
     }
   }
 
