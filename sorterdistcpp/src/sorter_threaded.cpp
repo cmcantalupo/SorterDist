939d0af878210c589cf713d43275ba333e3d52db
Christopher Cantalupo <cmcantalupo@lbl.gov>
2011-07-06 14:31:42 -0700

    Got rid of PartitionGang and sort now uses Partition and Splinter

--- sorterdistcpp/src/sorter_threaded.cpp
+++ sorterdistcpp/src/sorter_threaded.cpp
@@ -2,9 +2,9 @@
 // C.M.Cantalupo 
 
 #include "sorter_threaded.hpp"
-#include "partition_gang.hpp"
+#include "partition.hpp"
+#include "splinter.hpp"
 #include "sorter_threaded_exception.hpp"
-
 
 void SorterThreaded::sort(std::vector<double>::iterator begin, 
                           std::vector<double>::iterator end) {
@@ -29,20 +29,77 @@ void SorterThreaded::sort(std::vector<double>::iterator begin,
   // bound function so we need to overload less than.
   // 
 
-  try {
-    SorterThreadedHelper::PartitionGang gang(begin, end, numThreads_, taskFactor_);
-    gang.fill();
-    gang.pop();
-    gang.sort();
+#ifndef _OPENMP
+  std::sort(begin, end);
+#else
+  if (numThreads_ == -1) {
+    size_t numThreads = omp_get_max_num_threads();
+  }
 
+  numTasks = numThreads_ * taskFactor_;
+  std::vector<double>::iterator chunkBegin;
+  std::vector<double>::iterator chunkEnd;
+  std::set<double> pivots;
+
+  // Create a set of pivots by going through the vector    
+  for (std::vector<double>::iterator it = begin;
+       it != end && pivots.size() < numThreads_*taskFactor_ - 1;
+       ++it) {
+      pivots.insert(*it);
   }
-  catch (SorterThreadedException& e) {
-    if(e.error == SorterThreadedException::TooFewPivots ||
-       e.error == SorterThreadedException::NoOpenMP)
-      std::sort(begin, end);
-    else
-      throw(e);
+  // Check that there were as many unique values as we need
+  if (pivots.size() < numThreads_ * taskFactor_ - 1) {
+    std::sort(begin, end);
+    return;
   }
+  
+  Splinter splinter(begin, end);
+  std::vector<Partition*> allPartitions(numThreads);
+  std::vector<std::vector<double>::iterator> chunks;
+
+  size_t i = 0;
+  for (std::vector<Partition*>::iterator it = allPartitions.begin();
+       it != allPartitions.end(); ++it, ++i) {
+    *it = new Partition(pivots);
+  }
+
+  splinter.even(numThreads, chunks);
+
+#pragma omp parallel default (none) private (none) shared (allPartitions, splinter, chunks)
+{
+  int my threadID = omp_get_thread_num();
+  allPartitions[threadID]->fill(chunks[myThreadID], chunks[myThreadID+1]);  
+  omp_barrier();
+  std::vector<size_t> mySizes;
+  allPartitions[threadID]->taskSizes(mySizes);
+
+  for (size_t i = 0; i < numThreads; ++i) {
+    if (i == threadID) {
+      splinter.addSizes(mySizes);
+    }
+    omp_barrier();
+  }
+
+  for (size_t i = 0; i < numThreads; ++i) {
+    if (i == threadID) {
+      getOffsets(mySizes, offsets);
+    }
+    omp_barrier();
+  }
+  
+  for (size_t i = 0; i < numTasks; ++i) {
+    allPartitions[threadID]->popTask(offsets[i], offsets[i+1]);
+  }
+
+  if (threadID == numThreads-1) {
+    taskOffsets = offsets;
+  }
+
+#pragma omp parallel for schedule (dynamic)
+  for (i = 0; i < numTasks; ++i) {
+    std::sort(taskOffsets[i], taskOffsets[i+1]);
+  }
+#endif
 }
 
 SorterThreaded::SorterThreaded(size_t taskFactor, size_t numThreads) :
