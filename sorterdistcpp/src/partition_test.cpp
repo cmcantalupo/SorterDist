00f41e5432834c177863cf3c4c85e3db25d3f6eb
Christopher Cantalupo <cmcantalupo@lbl.gov>
2011-07-05 17:50:31 -0700

    Extremely crude test for Partition needs work.

---
+++ sorterdistcpp/src/partition_test.cpp
@@ -0,0 +1,52 @@
+#include "partition.hpp"
+#include "sorter_threaded_exception.hpp"
+#include <iostream>
+#include <assert.h>
+
+using namespace SorterThreadedHelper;
+
+int main(int argc, char **argv) {
+  const double testArr[18] = {7, 15, 13, 17, 2, 10, 12, 3, 14, 9, 0, 8, 1, 11, 16, 6, 5, 4};
+  std::vector<double> test(18);
+  for (int i = 0; i < 18; i++) {
+    test[i] = testArr[i];
+  }
+  std::set<double> pivots;
+  pivots.insert(6);
+  pivots.insert(12);
+  
+
+  Partition part(pivots);
+  try {
+    part.fill(test.begin(), test.end());
+  }
+  catch(SorterThreadedException &e) {
+    std::cerr << "Partition::fill error SorterThreadedException: " << e.error << '\n';
+    return e.error;
+  }
+
+  size_t numTasks = part.numTasks();
+  std::vector<size_t> taskSizes(numTasks);
+
+  try {
+    part.taskSizes(taskSizes.begin(), taskSizes.end());
+  }
+  catch(SorterThreadedException &e) {
+    std::cerr << "Partition::taskSizes error SorterThreadedException: " << e.error << '\n';
+    return e.error;
+  }
+
+  std::vector<double> task(6);
+
+  for (int i = 0; i < numTasks; ++i) {
+    assert(i == part.curTask());
+    assert(taskSizes[i] == 6);
+    part.popTask(task.begin(), task.end());
+    for (int j = 0; j < taskSizes[i]; j++ ) {
+      if (i == 0) assert(task[j] < 6);
+      if (i == 1) assert(6 <= task[j] && task[j] < 12);
+      if (i == 2) assert(12 <= task[j]);
+    }
+  }
+
+}
