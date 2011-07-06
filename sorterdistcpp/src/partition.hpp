8c98a1a04060f49941e1f576be4d0a3d05f644e9
Christopher Cantalupo <cmcantalupo@lbl.gov>
2011-07-05 17:46:36 -0700

    Cleaned up Partition interface.  Moved the chunk out of the constructor into the fill

--- sorterdistcpp/src/partition.hpp
+++ sorterdistcpp/src/partition.hpp
@@ -7,28 +7,49 @@
 #include "partition_wall.hpp"
 
 namespace SorterThreadedHelper {
-
+  // Each thread will have a partition and the members will
+  // be single threaded functions.  
   class Partition {
     public:
-      Partition(const std::set<double>& pivots,
-                const std::vector<double>::iterator chunkBegin, 
-                const std::vector<double>::iterator chunkEnd);
-      ~Partition(); 
-      void fillPartition(); //single threaded
-      void popTask(std::vector<double>::iterator resultBegin);
-      void stackSizes(std::vector<size_t>::iterator begin, 
-                      std::vector<size_t>::iterator end);
+      // A partition is created with a set of pivots.  There are
+      // pivots.size() + 1 tasks. Each task is a stack of values less
+      // than the pivot for the first tasks, and the last task is a
+      // stack of values larger than any of the pivots.
+      Partition(const std::set<double>& pivots);
+      Partition(const Partition& other);
+      ~Partition();
+
+      // Pushes all of the values in a chunk onto the partition
+      // stacks.
+      void fillPartition(std::vector<double>::const_iterator begin,
+                         std::vector<double>::const_iterator end);
+
+      // Returns all of the values in the current task.  
+      void popTask(std::vector<double>::iterator begin,
+                   std::vector<double>::iterator end);
+
+      // Returns the number of tasks (the size of the original pivot
+      // set plus one).
+      size_t numTasks();
+
+      // Returns the index of the next task that will be popped by 
+      // popTask().  
+      size_t curTask();
+
+      // Returns the size of the next task that will be popped by 
+      // popTask().
+      size_t curSize();
+
+      // Returns the sizes of all of the tasks.  
+      void taskSizes(std::vector<size_t>::iterator begin, 
+                     std::vector<size_t>::iterator end);
 
     private:
-      const std::vector<double>::iterator chunkBegin_;
-      const std::vector<double>::iterator chunkEnd_;
+      size_t numTasks_;
+      size_t curTask_;
       std::map<PartitionWall,std::stack<double>*> partition_;
-      std::map<PartitionWall,std::stack<double>*>::iterator taskIt_;
-
+      std::map<PartitionWall,std::stack<double>*>::iterator curTaskIt_;
   };
-
-
-
 }
 
 #endif
