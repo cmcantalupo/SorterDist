cfa2d7dde51ea82ccca0b8c629e9ce458834fbe0
Christopher Cantalupo <cmcantalupo@lbl.gov>
2011-07-07 15:54:04 -0700

    adding a crude test for SorterThreaded

---
+++ sorterdistcpp/src/sorter_threaded_test.cpp
@@ -0,0 +1,21 @@
+#include "sorter_threaded.hpp"
+#include <algorithm>
+#include <assert.h>
+
+int main(int argc, char *argv) {
+  size_t testSize = 1000;
+  std::vector<double> orderedVector(testSize);
+  SorterThreaded st;
+  
+  for (size_t i = 0; i < testSize; i++) {
+    orderedVector[i] = static_cast<double>(i);
+  }
+  std::vector<double> testVector(orderedVector);
+
+  random_shuffle(testVector.begin(), testVector.end());
+  
+  st.sort(testVector.begin(), testVector.end());
+  
+  assert(testVector == orderedVector);
+    
+}
