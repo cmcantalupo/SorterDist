ffca68d14e871e3d0b44914e61eb5cad84582467
Christopher Cantalupo <cmcantalupo@lbl.gov>
2011-07-08 12:09:08 -0700

    std::sort is not thread safe.  We need our own sort, so I added a crude quick_sort should be improved with STL partition and made a template

---
+++ sorterdistcpp/src/quick_sort.hpp
@@ -0,0 +1,8 @@
+#ifndef quick_sort_hpp
+#define quick_sort_hpp
+#include <vector>
+
+void quick_sort(std::vector<double>::iterator begin, 
+                std::vector<double>::iterator end);
+
+#endif
