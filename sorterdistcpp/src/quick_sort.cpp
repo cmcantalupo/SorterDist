ff61a4e3aa05bf5a3b5f8e390fbf4cfd050311f2
Christopher Cantalupo <cmcantalupo@lbl.gov>
2011-07-08 18:54:03 -0700

    faster but broken quick_sort

--- sorterdistcpp/src/quick_sort.cpp
+++ sorterdistcpp/src/quick_sort.cpp
@@ -1,75 +1,32 @@
 #include "quick_sort.hpp"
 
-void quick_sort(std::vector<double>::iterator begin, 
+
+std::vector<double>::iterator part(std::vector<double>::iterator begin, 
+                                   std::vector<double>::iterator end, double value) {
+  // cribbed from the explaination of STL partition
+  while (true) {
+    while (begin != end && *begin < value) ++begin;
+    if (begin == end--) break;
+    while (begin != end && !(*begin < value)) --end;
+    if (begin == end) break;
+    std::swap(*begin++, *end);
+  }
+  return begin;
+}
+
+void mquick_sort(std::vector<double>::iterator begin, 
                 std::vector<double>::iterator end) {
   // Recursive implementation of the quick sort algorithm
-  std::vector<double>::iterator it;
-  double beginValue;
-  long int smallCount = 0;
-  long int bigCount = 0;
-  long int totalCount = std::distance(begin, end);
-  long int smallIndex = 0;
-  long int bigIndex = 0;
-
   // If the vector has less than two elements return
-  if ( totalCount < 2 ){
+  if ( distance(begin, end) < 2 ){
     return;
   }
 
-  // Iterate through the list and count the number of elements 
-  // smaller than the first element
-  beginValue = *begin;
-  it = begin;
-  ++it;
-  for (; it < end; ++it){
-    if (*it < beginValue) {
-      ++smallCount;
-    }
-  }
-  bigCount = totalCount - smallCount - 1;
-  it = begin;
-  ++it;
-  
-  // Create two new vectors to hold the values smaller and larger 
-  // than the first element
-  std::vector<double> smallVec(smallCount);
-  std::vector<double> bigVec(bigCount);
-
-
-  // Fill the new vectors
-  it = begin;
-  ++it;
-  for (; it < end; ++it) {
-    if (*it < beginValue) {
-      smallVec[smallIndex] = *it;
-      ++smallIndex;
-    }
-    else {
-      bigVec[bigIndex] = *it;
-      ++bigIndex;
-    }
-  }
-  
-  // If the small vector has more than one element recursively call 
-  // quickSort on it
-  if (smallCount > 1) {
-    quick_sort(smallVec.begin(), smallVec.end());
-  }
-  // If the large vector has more than one element recursively call 
-  // quickSort on it
-  if (bigCount > 1) {
-    quick_sort(bigVec.begin(), bigVec.end());
-  }
-  
-  // Now that the big and small list are sorted fill the input with 
-  // the results
-  it = begin;
-  for (smallIndex = 0; smallIndex < smallCount; ++smallIndex, ++it) {
-    *it = smallVec[smallIndex];
-  }
-  *it = beginValue;
-  ++it;
-  for (bigIndex = 0; bigIndex < bigCount; ++bigIndex, ++it) {
-    *it = bigVec[bigIndex];
-  }
+  std::vector<double>::iterator bound;
+  double beginVal = *begin;
+  bound = part(begin+1, end, beginVal);
+  copy(begin+1, bound, begin);
+  *bound = beginVal;
+  mquick_sort(begin, bound);
+  mquick_sort(bound+1, end);
 }
--- sorterdistcpp/src/quick_sort.hpp
+++ sorterdistcpp/src/quick_sort.hpp
@@ -2,7 +2,7 @@
 #define quick_sort_hpp
 #include <vector>
 
-void quick_sort(std::vector<double>::iterator begin, 
+void mquick_sort(std::vector<double>::iterator begin, 
                 std::vector<double>::iterator end);
 
 #endif
