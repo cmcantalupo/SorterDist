6a3c05f2b9f9524a698710bb93407d97b0f7d92d
Christopher Cantalupo <cmcantalupo@lbl.gov>
2011-07-08 21:52:13 -0700

    test of quick_sort

---
+++ sorterdistcpp/src/quick_sort_test.cpp
@@ -0,0 +1,21 @@
+#include "quick_sort.hpp"
+#include <vector>
+#include <algorithm>
+#include <assert.h>
+
+int main(int argc, char **argv) {
+  size_t testSize = 10000;
+  std::vector<double> orderedVector(testSize);
+  
+  for (size_t i = 0; i < testSize; i++) {
+    orderedVector[i] = static_cast<double>(i);
+  }
+  std::vector<double> testVector(orderedVector);
+
+  random_shuffle(testVector.begin(), testVector.end());
+  
+  mquick_sort(testVector.begin(), testVector.end());
+
+  assert(testVector == orderedVector);
+    
+}
