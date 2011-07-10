ac41e4f3b7b5dda975fe3e277c112cb79a2899b6
Christopher Cantalupo <cmcantalupo@gmail.com>
2011-07-09 18:01:22 -0700

    moved profile initialize and finalize

--- sorterdistcpp/src/sorter_threaded_test.cpp
+++ sorterdistcpp/src/sorter_threaded_test.cpp
@@ -1,9 +1,15 @@
 #include "sorter_threaded.hpp"
 #include <algorithm>
 #include <assert.h>
-
+#ifdef PROFILE_STT
+#include "M3_profile.h"
+#endif 
 
 int main(int argc, char **argv) {
+#ifdef PROFILE_STT
+  M3_ProfileInit("sorter_threaded_test");
+#endif
+
   size_t testSize = 1000000;
   std::vector<double> orderedVector(testSize);
   SorterThreaded st;
@@ -14,9 +20,22 @@ int main(int argc, char **argv) {
   std::vector<double> testVector(orderedVector);
 
   random_shuffle(testVector.begin(), testVector.end());
-  
+
+#ifdef PROFILE_STT
+  M3_ProfileStart(0, "sort 1 million");
+#endif 
+
   st.sort(testVector.begin(), testVector.end());
 
+#ifdef PROFILE_STT
+  M3_ProfileStop(0);
+#endif
+
   assert(testVector == orderedVector);
+
+#ifdef PROFILE_STT
+  M3_ProfileFinalize();
+#endif
+
     
 }
