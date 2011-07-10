784a4a07b2395ffe2e5c0264a38ae882aa9e535e
Christopher Cantalupo <cmcantalupo@gmail.com>
2011-07-09 18:52:49 -0700

    added library to Makefile and put profiling in its own code

--- sorterdistcpp/src/sorter_threaded_test.cpp
+++ sorterdistcpp/src/sorter_threaded_test.cpp
@@ -1,15 +1,9 @@
 #include "sorter_threaded.hpp"
 #include <algorithm>
 #include <assert.h>
-#ifdef PROFILE_STT
-#include "M3_profile.h"
-#endif 
+
 
 int main(int argc, char **argv) {
-#ifdef PROFILE_STT
-  M3_ProfileInit("sorter_threaded_test");
-#endif
-
   size_t testSize = 1000000;
   std::vector<double> orderedVector(testSize);
   SorterThreaded st;
@@ -21,21 +15,8 @@ int main(int argc, char **argv) {
 
   random_shuffle(testVector.begin(), testVector.end());
 
-#ifdef PROFILE_STT
-  M3_ProfileStart(0, "sort 1 million");
-#endif 
-
   st.sort(testVector.begin(), testVector.end());
-
-#ifdef PROFILE_STT
-  M3_ProfileStop(0);
-#endif
 
   assert(testVector == orderedVector);
 
-#ifdef PROFILE_STT
-  M3_ProfileFinalize();
-#endif
-
-    
 }
